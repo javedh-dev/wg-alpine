e48dda978d4145dc390e5a98ebf4d51008b8cfc8
Javed Hussain <javed15895@gmail.com>
2023-07-22 12:06:51 +0530

    added os and package manager check

--- install.sh
+++ install.sh
@@ -22,8 +22,46 @@ update_alpine(){
     #sudo apt-get update && sudo apt-get upgrade -y
 }
 
+is_command() {
+    # Checks to see if the given command (passed as a string argument) exists on the system.
+    # The function returns 0 (success) if the command exists, and 1 if it doesn't.
+    local check_command="$1"
+
+    command -v "${check_command}" >/dev/null 2>&1
+}
+
+os_check() {
+    detected_os=$(grep "\bID\b" /etc/os-release | cut -d '=' -f2 | tr -d '"')
+    detected_version=$(grep VERSION_ID /etc/os-release | cut -d '=' -f2 | tr -d '"')
+    if ! grep -iq "alpine" <(echo "$detected_os"); then
+        printf "%b This installer only supports Alpine Linux." "${INFO}"
+        printf "\n%b ${detected_os} is not yet supported\n\n" "${CROSS}"
+        exit 1
+    fi
+}
+
+package_manager_detect() {
+if is_command apk ; then
+    PKG_MANAGER="apk"
+    UPDATE_PKG_CACHE="${PKG_MANAGER} update"
+    PKG_INSTALL=("${PKG_MANAGER}" add)
+    PKG_COUNT="${PKG_MANAGER} upgrade --simulate --no-progress | head -n -1 | wc -l"
+    INSTALLER_DEPS=(git openrc grep tar)
+    WG_DEPS=(wget)
+# If apk package managers was not found
+else
+    # we cannot install required packages
+    printf "  %b No supported package manager found\\n" "${CROSS}"
+    # so exit the installer
+    exit
+fi
+}
+
+
 start_setup() {
     show_ascii_logo
+    os_check
+    package_manager_detect
     printf "\n\n%b Setup will install Wireguard VPN with wireguard UI on alpine linux" "${INFO}"
     printf "\n%b Initializing Wireguard on alpine" "${TICK}"
     update_alpine
